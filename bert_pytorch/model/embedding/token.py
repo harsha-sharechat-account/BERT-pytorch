7707af5fb3dd68fe39510f7f672b7435c9391107
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-11 14:15:10 +0000

    changes

--- bert_pytorch/model/embedding/token.py
+++ bert_pytorch/model/embedding/token.py
@@ -1,6 +1,19 @@
 import torch.nn as nn
+import numpy as np
+import torch
 
-
-class TokenEmbedding(nn.Embedding):
+# class TokenEmbedding(nn.Embedding):
+#     def __init__(self, vocab_size, embed_size=512):
+#         super().__init__(vocab_size, embed_size, padding_idx=0)
+class TokenEmbedding(nn.Module):
     def __init__(self, vocab_size, embed_size=512):
-        super().__init__(vocab_size, embed_size, padding_idx=0)
+        super().__init__()
+        path='/home/sriharshkamma/final/BERT-pytorch/fv.npy'
+        embedding_array=np.load(path)
+        self.embedding_dim=embed_size
+        self.embedding=nn.Embedding(vocab_size, embed_size, padding_idx=0)
+        self.embedding.weight.data.copy_(torch.from_numpy(embedding_array))
+        
+    def forward(self,x):
+        return self.embedding(x)
+        
